15829665fa1eacfd5e8e8df66004f46f7e23b634
soolaf <harkysoolaf@gmail.com>
2019-07-26 12:27:27 +0200

    Sets the request and body validation

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const connectDB = require('./config/db');
 const app = express();
 //connect Databases
 connectDB();
+//Init Middleware
+app.use(express.json({ extended: false }));
 app.get('/', (req, res) => res.send('API Running'));
 //Define Routes
 app.use('/api/users', require('./routes/api/users'));
