9355b070de86e7b8d101b262dccd680fbe56ae35
soolaf <harkysoolaf@gmail.com>
2019-07-27 11:53:02 +0200

    implements jsonwebtoken and sets custom auth middleware & JWT verify

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 const User = require('../../models/User');
 
@@ -43,7 +45,15 @@ router.post(
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
       await user.save();
-      res.send('User registered');
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 360000 }, (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      });
     } catch (err) {
       console.error(err.message);
       res.status(400).send('Server error');
